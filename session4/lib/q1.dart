aef4a228274ac65911be2d7d3d10f5e6e06619fd
ElsayedMagdy99 <elsayedmagdy20520000@gmail.come>
2025-03-19 16:08:51 +0200

    done

---
+++ session4/lib/q1.dart
@@ -0,0 +1,17 @@
+/*Create a Dart program that declares and initializes the following variables:
+- Your name (String)
+- Your age (int)
+- Your height (double)
+- Whether you are a student (bool)
+Then, print these values using a single print statement.*/
+
+void main() {
+  String name = 'Elsayed';
+  int age = 26;
+  double height = 173;
+  bool isYouAreStudent = false;
+  print(name);
+  print(age);
+  print(height);
+  print(isYouAreStudent);
+}
